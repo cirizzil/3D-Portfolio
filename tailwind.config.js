d5a89c3d5969d4110fc3c67b58b8c13cf0846990
Ciril Biju Joseph <cirilbijujoseph@gmail.com>
2024-07-31 11:30:39 +0400

    Push Files

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#000000",
+        secondary: "#FFFFFF",//"#453E36"
+        tertiary: "#0d0d0d",//"#1B1B1B",
+        "black-100": "#1B1B1B",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg1.png')",
+      },
+    },
+  },
+  plugins: [],
+};

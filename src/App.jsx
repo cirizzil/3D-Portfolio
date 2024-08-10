5cd95dfdb3eca2ed868dbb278b7905998bc35b35
Ciril Biju Joseph <cirilbijujoseph@gmail.com>
2024-08-10 14:11:24 +0400

    Added Web Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
           <StarsCanvas />
         </div>
         <SpeedInsights />
+        <Analytics />
       </div>
     </BrowserRouter>
   );
